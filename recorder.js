eb35cabb49a23d70eba30b1286aa8c2e76127bd0
Clayton Salem <claytonjsalem@gmail.com>
2019-01-29 14:44:22 -0500

    Adds the waveform

--- recorder.js
+++ recorder.js
@@ -1,107 +1,74 @@
-// const record = document.querySelector('#record');
-// const stop = document.querySelector('#stop');
-// const soundClips = document.querySelector('#sound-clips');
-// const canvas = document.querySelector('#visualizer');
-// const mainSection = document.querySelector('#main-controls');
-
-// // disable stop button while not recording
-
-// stop.disabled = true;
-
-// // visualiser setup - create web audio api context and canvas Not done yet.
-
-// // const audioCtx = new (window.AudioContext || webkitAudioContext)();
-// // const canvasCtx = canvas.getContext("2d");
-
-// //main block for doing the audio recording
-
-// if (navigator.mediaDevices.getUserMedia) {
-//     console.log('getUserMedia supported.');
-
-//     let chunks = [];
-
-//     const onSuccess = (stream) => {
-
-//         const mediaRecorder = new MediaRecorder(stream);
-
-//         record.onclick = function () {
-//             mediaRecorder.start();
-//             stop.disabled = false;
-//             record.disabled = true;
-//             canvas.innerText = 'Cool waveform indicates recording here.'
-//         }
-
-//         stop.onclick = function () {
-//             mediaRecorder.stop();
-//             stop.disabled = true;
-//             record.disabled = false;
-//             canvas.innerText = ''
-//         }
-
-//         mediaRecorder.onstop = function (e) {
-//             let clipContainer = document.createElement('article');
-//             let audio = document.createElement('audio');
-//             let deleteButton = document.createElement('button');
-//             let saveButton = document.createElement('button')
-
-//             clipContainer.classList.add('clip');
-//             audio.setAttribute('controls', '');
-//             deleteButton.textContent = 'Delete';
-//             deleteButton.className = 'delete';
-//             saveButton.textContent = 'Save File';
-//             saveButton.className = 'save';
-
-//             clipContainer.appendChild(audio);
-//             clipContainer.appendChild(deleteButton);
-//             clipContainer.appendChild(saveButton)
-//             soundClips.appendChild(clipContainer);
-
-//             audio.controls = true;
-//             let blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
-//             chunks = [];
-//             let audioURL = window.URL.createObjectURL(blob);
-//             audio.src = audioURL;
-//             console.log("recorder stopped");
-
-//             deleteButton.onclick = (e) => {
-//                 evtTgt = e.target;
-//                 evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-//             }
-
-//             saveButton.onclick = (e) => {
-//                 console.log(blob)
-//                 alert(`Save sends this ${blob.size}-bit audio blob to server`)
-//             }
-//         }
-
-//         mediaRecorder.ondataavailable = function (e) {
-//             chunks.push(e.data);
-//         }
-//     } // onSuccess
-
-//     const onError = (err) => {
-//         console.log('The following error occured: ' + err);
-//     }
-
-//     navigator.mediaDevices.getUserMedia({ audio: true }).then(onSuccess, onError);
-
-// } else {
-//     console.log('getUserMedia not supported on your browser!');
-// }
-
 let recorder
+let source
 const recordButton = document.querySelector("#record")
 const stopButton = document.querySelector("#stop")
 const audio = document.querySelector("audio")
+const saveButton = document.querySelector("#save")
+const canvas = document.querySelector("#visualizer")
+const canvasCtx = canvas.getContext("2d")
+const url = 'savepointurl'
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+const analyser = audioCtx.createAnalyser()
+const WIDTH = 300
+const HEIGHT = 50
+
 
 recordButton.addEventListener('click', () => {
+    audioCtx.resume()
     // Request permissions to record audio
     navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
         recorder = new MediaRecorder(stream)
+        source = audioCtx.createMediaStreamSource(stream)
+        source.connect(analyser)
+        analyser.fftSize = 2048;
+        let bufferLength = analyser.frequencyBinCount;
+        let dataArray = new Uint8Array(bufferLength);
+
+        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+        const draw = () => {
+            requestAnimationFrame(draw);
+            analyser.getByteTimeDomainData(dataArray)
+            canvasCtx.fillStyle = 'rgb(255, 244, 205)';
+            canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+            canvasCtx.lineWidth = 2;
+            canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+            canvasCtx.beginPath();
+            // line width
+            let sliceWidth = WIDTH * 1.0 / bufferLength;
+            let x = 0;
+            for (let i = 0; i < bufferLength - 1; i++) {
+
+                let v = dataArray[i] / 128.0;
+
+                let y = v * (HEIGHT / 2);
+
+                if (i == 0) {
+                    canvasCtx.moveTo(x, y);
+                } else {
+                    canvasCtx.lineTo(x, y);
+                }
+
+                x += sliceWidth;
+            }
+            canvasCtx.lineTo(canvas.width, canvas.height / 2);
+            canvasCtx.stroke();
+
+        }
+        draw();
+
 
         // Set record to <audio> when recording will be finished
         recorder.addEventListener('dataavailable', e => {
+            // console.log(e.data);
             audio.src = URL.createObjectURL(e.data)
+            // Add save event listener and send to server.
+            saveButton.addEventListener('click', () => {
+                // console.log(e.data)
+                fetch(url, { method: 'POST', body: e.data })
+                    .then(res => { alert('posted recording') })
+                    .catch(err => alert('error ' + err))
+            })
         })
 
         // Start recording
@@ -115,3 +82,4 @@ stopButton.addEventListener('click', () => {
     // Remove “recording” icon from browser tab
     recorder.stream.getTracks().forEach(i => i.stop())
 })
+
